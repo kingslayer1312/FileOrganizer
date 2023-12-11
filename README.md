edcb4edc072146745cbf29f69f009a3f6d5fc761
Kingslayer1312 <hrishabari@gmail.com>
2023-12-11 22:09:01 +0530

    completed script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FileOrganizer
+# FileOrganizer  
+
+## A simple python script that organizes all files in a directory into subfolders based on their extension  
+
+Note: As of now, the script has been designed to work on UNIX or UNIX-like operating systems only.
---
+++ file_organizer.py
@@ -0,0 +1,44 @@
+import os
+import pathlib
+
+class FileOrganizer:
+    def printWelcomeMessage(self):
+        print("Welcome to FileOrganizer!\n")
+        print("FileOrganizer lets you organize all files of the same type into separate directories\n")
+    def changeDirectory(self, path):
+        try:
+            os.chdir(path)
+        except OSError:
+            print("Sorry, the directory does not exist\n")
+    def findFiles(self):
+        files = [file for file in os.listdir(".") if os.path.isfile(file)]
+        return files
+    def findExtensions(self, files):
+        extensions = []
+        for i in files:
+            if pathlib.Path(i).suffix not in extensions:
+                extensions.append(pathlib.Path(i).suffix[1:])
+        return set(extensions)
+    def createFolders(self, extensions):
+        for i in extensions:
+            os.mkdir(i)
+    def moveFiles(self, files, extensions):
+        for i in files:
+            if pathlib.Path(i).suffix[1:] in extensions:
+                os.rename(i, "{}/{}".format(pathlib.Path(i).suffix[1:], i))
+            else:
+                os.mkdir(pathlib.Path(i).suffix[1:])
+
+tool = FileOrganizer()
+
+tool.printWelcomeMessage()
+print("Enter file path: ")
+path = input()
+tool.changeDirectory(path)
+files = tool.findFiles()
+extensions = tool.findExtensions(files)
+print(extensions)
+tool.createFolders(extensions)
+tool.moveFiles(files, extensions)
+
+print("Operation done")
